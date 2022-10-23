22ccc6e8c051a7e027c8abe412ae3391e193de08
dodamce <3030949391@qq.com>
2022-10-23 09:04:13 +0800

    考虑内存泄漏问题

--- 数据结构/线性表/2-3课后习题/2-3-02带头节点单链表删除值为x的节点.cpp
+++ 数据结构/线性表/2-3课后习题/2-3-02带头节点单链表删除值为x的节点.cpp
@@ -3,11 +3,40 @@
 using namespace std;
 
 //遍历法删除链表值为x的节点
-Node *DelElemByTravel(Node *list, int target)
+Node *DelElemByTravel(Node *head, int target)
 {
+    Node *prev = nullptr;
+    Node *node = head;
+    while (node != nullptr)
+    {
+        if (node->_val == target)
+        {
+            if (node == head)
+            {
+                head = head->_next;
+                delete node;
+                node = head;
+            }
+            else
+            {
+                prev->_next = node->_next;
+                delete node;
+                node = prev->_next;
+            }
+        }
+        else
+        {
+            prev = node;
+            node = node->_next;
+        }
+    }
+    return head;
 }
 
 int main()
 {
-    NodeListHead list({2, 3, 1, 2, 3, 2, 2, 5, 6, 3, 2});
+    NodeListHead list({2, 2, 3, 1, 2, 3, 2, 2, 5, 6, 3, 2});
+    list.PrintList();
+    DelElemByTravel(list.head, 2);
+    list.PrintList();
 }
