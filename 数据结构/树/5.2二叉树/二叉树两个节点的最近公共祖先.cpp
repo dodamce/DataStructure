d8cdaa9110c887c89195332784ff06cf038862ce
dodamce <3030949391@qq.com>
2022-11-05 14:39:27 +0800

    寻找二叉树的最近公共父节点(递归)

--- 数据结构/树/5.2二叉树/二叉树两个节点的最近公共祖先.cpp
+++ 数据结构/树/5.2二叉树/二叉树两个节点的最近公共祖先.cpp
@@ -1,11 +1,45 @@
-#include "GetTreeByVector.h"
+//为了实现思想，这里选择leetcode的题
+// https://leetcode.cn/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/
 
-using namespace std;
+//  Definition for a binary tree node.
 
-int main()
+#include <iostream>
+
+struct TreeNode
 {
-    MyTree tree({1, 2, 3, 4, 5, 6, 7, 8, 9});
-    tree.PreDisplay();
-    tree.Print();
-    return 0;
-}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        if (root == nullptr)
+        {
+            return nullptr;
+        }
+        if (root == p)
+        {
+            return p;
+        }
+        else if (root == q)
+        {
+            return q;
+        }
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);//找左子树
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);//找右子树
+        if (left != nullptr && right != nullptr)
+        {
+            return root;
+        }
+        else if (left == nullptr)
+        {
+            return right;
+        }
+        return left;
+    }
+};
