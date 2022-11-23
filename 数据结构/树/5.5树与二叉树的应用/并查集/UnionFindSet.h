f930e361a52e8a457159846883ee6ca431cb570c
dodamce <3030949391@qq.com>
2022-11-23 11:42:53 +0800

    添加VS2022工程环境

---
+++ 数据结构/树/5.5树与二叉树的应用/并查集/UnionFindSet.h
@@ -0,0 +1,17 @@
+//构建并查集
+
+#include <vector>
+
+class UnionFindSet
+{
+private:
+    std::vector<int> ufs;
+
+public:
+    UnionFindSet(size_t size)
+    {
+        ufs.reserve(size, -1);
+        int
+    }
+    
+};
