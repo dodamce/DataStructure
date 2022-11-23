e70e3cbde4119634d5d65c4a476ea3510ebdc91e
dodamce <3030949391@qq.com>
2022-11-23 11:59:07 +0800

    C++实现并查集,并配置VS2022工程环境

--- 数据结构/树/5.5树与二叉树的应用/并查集/UnionFindSet.h
+++ 数据结构/树/5.5树与二叉树的应用/并查集/UnionFindSet.h
@@ -1,17 +1,51 @@
 //构建并查集
 
+#include <assert.h>
 #include <vector>
 
 class UnionFindSet
 {
 private:
+    //数组的下标保存的是并查集的数据，数组的值记录的是并查集这个节点的父节点下标
     std::vector<int> ufs;
 
 public:
     UnionFindSet(size_t size)
     {
         ufs.reserve(size, -1);
-        int
     }
-    
+
+    // x和y所在的两个集合合并
+    void Union(int x, int y)
+    {
+        assert(x < ufs.szie() && y < ufs.size());
+        int root_x = FindRoot(x);
+        int root_y = FindRoot(y);
+
+        if (root_x != root_y)
+        {
+            //不在一棵树上
+            ufs[root_x] += root_y;
+            ufs[root_y] = root_x;
+        }
+    }
+
+    //找data的根
+    int FindRoot(int data)
+    {
+        int root = data;
+        while (ufs[root] > 0)
+        {
+            root = ufs[root];
+        }
+
+        //找到根后，这里做优化，降低并查集树的高度
+        //把这个节点到根节点路径上的所有节点插入到根节点上
+        while (data != root)
+        {
+            int parent = ufs[data];
+            ufs[data] = root;
+            data = parent;
+        }
+    }
 };
