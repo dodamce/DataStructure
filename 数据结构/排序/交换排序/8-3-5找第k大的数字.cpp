b703d32126f7039c30fb9a6e2e41b7c649efc4c3
dodamce <3030949391@qq.com>
2023-03-09 21:21:54 +0800

    找顺序表第k大的数字基本思路

--- 数据结构/排序/交换排序/8-3-5找第k大的数字.cpp
+++ 数据结构/排序/交换排序/8-3-5找第k大的数字.cpp
@@ -1,2 +1,11 @@
-#include <iostream>
-#include <vector>
+/**
+ * @file 8-3-5找第k大的数字.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * 简单思路：排序后返回数组k位置的元素
+ * @version 0.1
+ * @date 2023-03-09
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
